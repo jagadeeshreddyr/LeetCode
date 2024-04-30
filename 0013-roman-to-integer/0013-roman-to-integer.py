43f4cff02fb7c7b5afc59098dbc9187af1c55902
Jagadeesh Reddy R <76152076+jagadeeshreddyr@users.noreply.github.com>
2024-04-30 16:20:42 +0530

    Time: 35 ms (96.38%), Space: 16.7 MB (19.51%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        
+        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+
+
+
+
+        count=d[s[-1]]
+
+        prev = d[s[-1]]
+
+        for i in range(len(s)-2, -1,-1):
+
+            if d[s[i]] >= prev:
+                count+=d[s[i]]
+                prev = d[s[i]]
+            else:
+
+                count -= d[s[i]]
+
+
+        return count
