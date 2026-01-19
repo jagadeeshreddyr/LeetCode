272a63196dfb4755fc7e9fb1db33eeb92e861884
Jagadeesh Reddy R <76152076+jagadeeshreddyr@users.noreply.github.com>
2026-01-19 10:03:20 +0530

    LC-can-place-flowers [Runtime: N/A; Memory: N/A]

--- array/can-place-flowers.py
+++ array/can-place-flowers.py
@@ -2,11 +2,14 @@ class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
         for i in range(1, len(flowerbed)-1):
-            if flowerbed[i-1] !=1 and flowerbed[i]!=1 and flowerbed[i+1]!=1:
-                flowerbed[i] = 1
+            if flowerbed[i-1] !=1 and flowerbed[i]!=1:
+                flowerbed[i-1] = 1
+                n = n-1
+            elif flowerbed[i] !=1 and flowerbed[i+1]!=1:
+                flowerbed[i+1] = 1
                 n = n-1
         
-        if n ==0:
-            return True
-        else:
-            return False
+            if n ==0:
+                return True
+            else:
+                return False
