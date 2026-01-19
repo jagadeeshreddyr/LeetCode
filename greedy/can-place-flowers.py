2e4d724f94865ee7cd52eb3e3d9ae1e91c74c364
Jagadeesh Reddy R <76152076+jagadeeshreddyr@users.noreply.github.com>
2026-01-19 09:54:44 +0530

    LC-can-place-flowers [Runtime: N/A; Memory: N/A]

---
+++ greedy/can-place-flowers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1] !=1 and flowerbed[i]!=1 and flowerbed[i+1]!=1:
+                flowerbed[i] = 1
+                n = n-1
+        
+        if n ==0:
+            return True
+        else:
+            return False
